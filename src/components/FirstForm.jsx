f3b55eeef5cb5f8e379a6f0692303c533fca6494
gautam khatri <buddhgautam777@gmail.com>
2024-04-24 14:16:01 +0530

    all done

---
+++ src/components/FirstForm.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const FirstForm = () => {
+  return (
+    <div>
+
+    
+      <div>
+
+      </div>
+
+      {/* image */}
+
+      <div>
+
+      </div>
+
+
+    </div>
+  )
+}
+
+export default FirstForm
--- src/components/MultiStepForm.jsx
+++ src/components/MultiStepForm.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { startNewInterview } from '../Redux/userSlice';
 import { useDispatch } from 'react-redux';
+import FirstForm from './FirstForm';
+import SecondForm from './SecondForm';
+import ThirdForm from './ThirdForm';
 
 const MultiStepForm = () => {
   const [step, setStep] = useState(1);
@@ -46,53 +49,26 @@ const MultiStepForm = () => {
     switch (step) {
       case 1:
         return (
+
           <div>
-            <h2>Step 1: Personal Information</h2>
-            <input
-              type="text"
-              name="firstName"
-              placeholder="First Name"
-              value={formData.firstName}
-              onChange={handleChange}
-            />
-            <input
-              type="text"
-              name="lastName"
-              placeholder="Last Name"
-              value={formData.lastName}
-              onChange={handleChange}
-            />
-            <button onClick={nextStep}>Next</button>
+             <FirstForm />
+          <button onClick={nextStep}>Next</button>
           </div>
+          
         );
       case 2:
         return (
-          <div>
-            <h2>Step 2: Contact Information</h2>
-            <input
-              type="email"
-              name="email"
-              placeholder="Email"
-              value={formData.email}
-              onChange={handleChange}
-            />
-            <button onClick={prevStep}>Previous</button>
-            <button onClick={nextStep}>Next</button>
-          </div>
+              <div>
+                <SecondForm />
+                <button onClick={nextStep}>Next</button>
+              </div>
         );
       case 3:
         return (
-          <div>
-            <h2>Step 3: Additional Details</h2>
-            <textarea
-              name="message"
-              placeholder="Message"
-              value={formData.message}
-              onChange={handleChange}
-            />
-            <button onClick={prevStep}>Previous</button>
-            <button onClick={handleSubmit}>Submit</button>
-          </div>
+           <div>
+            <ThirdForm />
+           <button onClick={handleSubmit}>Next</button>
+           </div>
         );
       default:
         return null;
